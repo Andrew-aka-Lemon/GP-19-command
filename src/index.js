027c120004c804ce5c1d784482c9c772284e0677
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-07-15 20:54:08 +0300

    half-way done header

---
+++ src/index.js
@@ -0,0 +1,16 @@
+(() => {
+  const refs = {
+    openMenuBtn: document.querySelector('.header-menu__button'),
+    closeMenuBtn: document.querySelector('.menu-close-btn'),
+    menu: document.querySelector('.mob-menu'),
+    body: document.querySelector('body'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('hidden');
+    refs.body.classList.toggle('prevent-scroll');
+  }
+})();
