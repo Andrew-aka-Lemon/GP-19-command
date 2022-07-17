e13526f884f9313c84bd61490ee7e22186fce6e0
BogdanMU <104568558+BogdanMU@users.noreply.github.com>
2022-07-17 13:57:46 +0300

    js header done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Maxim Bogdan - script for header mobile menu/////
+// Maxim Bogdan - script for header mobile menu and buy-now modal window/////
 (() => {
   const refs = {
     openMenuBtn: document.querySelector('.header-menu__button'),
@@ -6,20 +6,47 @@
     menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
     HeaderMenuOpen: document.querySelector('.header-menu__button'),
-    HeaderModalBtn: document.querySelector('.modal-button-header')
+    HeaderModalBtn: document.querySelector('.modal-button-header'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
+
   function toggleMenu() {
     refs.menu.classList.toggle('mob-menu--hide');
     refs.body.classList.toggle('prevent-scroll');
-    refs.HeaderMenuOpen.classList.toggle('hidden')
-    refs.HeaderModalBtn.classList.toggle('hidden')
-  }
-})();
+    refs.HeaderMenuOpen.classList.toggle('hidden');
+    refs.HeaderModalBtn.classList.toggle('hidden');
+    }
 
+ 
+
+
+
+const modalBtn = {
+  openModalBtn: document.querySelector(".modal-open"),
+  mobOpenModalBtn: document.querySelector(".modal-open-mob"),
+  closeModalBtn: document.querySelector(".close-modal-button-buynow"),
+  backdrop: document.querySelector(".backdrop-buynow"),
+  body: document.querySelector('body')
+}
+  
+modalBtn.openModalBtn.addEventListener('click', toggleModal);
+modalBtn.mobOpenModalBtn.addEventListener('click', handleModalButton);
+modalBtn.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal(){
+  modalBtn.backdrop.classList.toggle('hidden');
+  modalBtn.body.classList.toggle('no-scroll');
+}
+
+function handleModalButton() {
+  toggleModal();
+  toggleMenu();
+}
+ 
+})();
 ///////////////////EL - скрипт для головної модалки Buy Now//////////////////////
 
 // const refs = {
