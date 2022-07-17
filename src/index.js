fd1b0cf32c18205746e81e93fa4a3c607dfd8fff
Sasha.Solo <sasha.solomannik@gmail.com>
2022-07-17 12:11:41 +0300

    fix-js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
     menu: document.querySelector('.mob-menu'),
     body: document.querySelector('body'),
     HeaderMenuOpen: document.querySelector('.header-menu__button'),
-    HeaderModalBtn: document.querySelector('.modal-button-header')
+    HeaderModalBtn: document.querySelector('.modal-button-header'),
   };
 
   refs.openMenuBtn.addEventListener('click', toggleMenu);
@@ -15,26 +15,44 @@
   function toggleMenu() {
     refs.menu.classList.toggle('mob-menu--hide');
     refs.body.classList.toggle('prevent-scroll');
-    refs.HeaderMenuOpen.classList.toggle('hidden')
-    refs.HeaderModalBtn.classList.toggle('hidden')
+    refs.HeaderMenuOpen.classList.toggle('hidden');
+    refs.HeaderModalBtn.classList.toggle('hidden');
   }
 })();
 
 ///////////////////EL - скрипт для головної модалки Buy Now//////////////////////
 
 const refs = {
-    openModalBtn: document.querySelector(".modal-button"),
-    closeModalBtn: document.querySelector(".close-modal-button-buynow"),
-    backdrop: document.querySelector(".backdrop-buynow"),
+  openModalBtn: document.querySelector('.modal-button'),
+  closeModalBtn: document.querySelector('.close-modal-button-buynow'),
+  backdrop: document.querySelector('.backdrop-buynow'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+refs.backdrop.addEventListener('click', logBackdropClick);
+
+function toggleModal() {
+  refs.backdrop.classList.toggle('is-hidden');
+  refs.ищвн.classList.toggle('no-scroll');
+}
+
+///////////////////SashaSolo - скрипт для Contacts//////////////////////
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('.contacts__button'),
+    closeModalBtn: document.querySelector('.contacts-modal-close__button'),
+    backdrop: document.querySelector('.backdrop-contacts'),
+    body: document.querySelector('body'),
   };
-  
+
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  
-  refs.backdrop.addEventListener('click', logBackdropClick);
-  
+
   function toggleModal() {
     refs.backdrop.classList.toggle('is-hidden');
-    refs.ищвн.classList.toggle('no-scroll');
-
+    refs.body.classList.toggle('no-scroll');
   }
+})();
