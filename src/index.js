dc22bfb49dd267f2987173b818821b4b0282b96d
marinakravchuk <105381706+marinakravchuk@users.noreply.github.com>
2022-08-02 23:13:18 +0300

    second update

--- src/index.js
+++ src/index.js
@@ -151,3 +151,18 @@ document.addEventListener('click', e => {
     toggleMenu();
   }
 })();
+
+////////////////////////Kravchuk - scroll-up button\\\\\\\\\\\\\\\\\\\\
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
